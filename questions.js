e8876784aded9829635d6f125db312bedb429f6c
Daniel Reza <daniel.reza1007@gmail.com>
2020-11-06 16:37:09 -0600

    updating JS

---
+++ questions.js
@@ -0,0 +1,52 @@
+var questions = [
+    {
+        question: "What is Buddy Rich's primary stick grip that he swore by?",
+        choices: ["Match grip", "Traditional grip", "French grip", "Natural grip"],
+        answer: "Traditional grip",
+    },
+    {
+        question: "The official name of the drum stool you sit on is the ______________.",
+        choices: ["Stump", "Highchair", "Rocker", "Throne"],
+        answer: "Throne",
+    },
+    {
+        question: "Paradiddles, anyone?",
+        choices: ["RRLL LLRR", "RLRL LRLR", "RLRR LRLL", "RRRL LLLR"],
+        answer: "RLRR LRLL",
+    },
+    {
+        question: "Which of these is not a material drum shells are made of?",
+        choices: ["Bronze", "Elephant bone", "Maple wood", "Acrylic"],
+        answer: "Elephant bone",
+    },
+    {
+        question: "John Bonham was the drummer for which band?",
+        choices: ["Led Zeppelin", "The Beatles", "Van Halen", "Nickelback"],
+        answer: "Led Zeppelin",
+    },
+    {
+        question: "What is a drummer's #1 tool to tune drums?",
+        choices: ["Drum fork", "Drum dial", "Drum key", "Drum stick"],
+        answer: "Drum key",
+    },
+    {
+        question: "What parts of the drum kit do you play in the pocket with?",
+        choices: ["Stick, foot, and hands", "Crash, ride, and hi-hat", "Throne, pedal, and sticks", "Kick, snare, and hi-hat"],
+        answer: "Kick, snare, and hi-hat",
+    },
+    {
+        question: "Who is the crazy drummer on the Muppet Show?",
+        choices: ["Kermit", "Animal", "Gonzo", "Beaker"],
+        answer: "Animal",
+    },
+    {
+        question: "Part of this band's fame comes from their one-armed drummer, Rick Allen:",
+        choices: ["Gun n Roses", "AC/DC", "Def Leppard", "Queen"],
+        answer: "Def Leppard",
+    },
+    {
+        question: "I've got a fever and the only prescription is _____________.",
+        choices: ["Rock n Roll", "More cowbell", "Drums", "Acetaminophen"],
+        answer: "More cowbell",
+    },
+];
--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 // Screen changes to question once user clicks on Start button
+
+var renderQuestion = document.getElementById("question");
+var renderOptions = document.querySelector(".options");
+
 function startFunction() {
     var startScreen = document.getElementById("start-screen");
     var questionStart = document.querySelector(".full-question");
@@ -9,6 +13,7 @@ function startFunction() {
     startScreen.style.display = "none";
     questionStart.style.display = "block";
     setTime();
+    questionList();
   }
 }
 
@@ -16,6 +21,7 @@ function startFunction() {
 var timer = document.getElementById("timer");
 
 var secondsLeft = 10;
+var index = 0;
 
 function setTime() {
     var timerInterval = setInterval(function() {
@@ -29,57 +35,23 @@ function setTime() {
     }, 1000)
 }
 
-// questions and answers 
-var questions = [
-    {
-        question: "What is Buddy Rich's primary stick grip that he swore by?",
-        choices: ["Match grip", "Traditional grip", "French grip", "Natural grip"],
-        answer: "Traditional grip",
-    },
-    {
-        question: "The official name of the drum stool you sit on is the ______________.",
-        choices: ["Stump", "Highchair", "Rocker", "Throne"],
-        answer: "Throne",
-    },
-    {
-        question: "Paradiddles, anyone?",
-        choices: ["RRLL LLRR", "RLRL LRLR", "RLRR LRLL", "RRRL LLLR"],
-        answer: "RLRR LRLL",
-    },
-    {
-        question: "Which of these is not a material drum shells are made of?",
-        choices: ["Bronze", "Elephant bone", "Maple wood", "Acrylic"],
-        answer: "Elephant bone",
-    },
-    {
-        question: "John Bonham was the drummer for which band?",
-        choices: ["Led Zeppelin", "The Beatles", "Van Halen", "Nickelback"],
-        answer: "Led Zeppelin",
-    },
-    {
-        question: "What is a drummer's #1 tool to tune drums?",
-        choices: ["Drum fork", "Drum dial", "Drum key", "Drum stick"],
-        answer: "Drum key",
-    },
-    {
-        question: "What parts of the drum kit do you play in the pocket with?",
-        choices: ["Stick, foot, and hands", "Crash, ride, and hi-hat", "Throne, pedal, and sticks", "Kick, snare, and hi-hat"],
-        answer: "Kick, snare, and hi-hat",
-    },
-    {
-        question: "Who is the crazy drummer on the Muppet Show?",
-        choices: ["Kermit", "Animal", "Gonzo", "Beaker"],
-        answer: "Animal",
-    },
-    {
-        question: "Part of this band's fame comes from their one-armed drummer, Rick Allen:",
-        choices: ["Gun n Roses", "AC/DC", "Def Leppard", "Queen"],
-        answer: "Def Leppard",
-    },
-    {
-        question: "I've got a fever and the only prescription is _____________.",
-        choices: ["Rock n Roll", "More cowbell", "Drums", "Acetaminophen"],
-        answer: "More cowbell",
-    },
-];
+function questionList() {
 
+var questionList = questions.length -1;
+
+if(index <= questionList) {
+    renderQuestion.innerHTML = questions[index].question;
+    optionsList();
+} else {
+    clearInterval(timerInterval);
+}
+};
+function optionsList() {
+    var options = questions[index].choices;
+    for (var a = 0; a < options.length; a++) {
+        var optionBtn = document.createElement("button");
+        optionBtn.className = "btn btn-primary";
+        optionBtn.innerHTML = options[a];
+        renderOptions.append(optionBtn);
+    }
+}
