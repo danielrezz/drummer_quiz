b9d2d37581b1d2a959fb20850e4feb0f03869d04
Daniel Reza <daniel.reza1007@gmail.com>
2021-07-16 13:19:17 -0500

    finished updating

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 var renderQuestion = document.getElementById("question");
 var renderOptions = document.querySelector(".options");
 var timer = document.querySelector("#timer");
+var questionStart = document.querySelector(".full-question");
+var startScreen = document.getElementById("start-screen");
 var totalSeconds = 10;
 var secondsElapsed = 10;
 var index = 0;
 var interval;
 var finalScore = 0;
-// var optionBtn = document.createElement("button");
-// optionBtn.className = "btn btn-primary";
+var scores = [];
 
 function startFunction() {
-  var startScreen = document.getElementById("start-screen");
-  var questionStart = document.querySelector(".full-question");
   var timer = document.querySelector("#timer");
 
   if (startScreen.style.display === "none") {
@@ -68,8 +67,8 @@ function startTimer() {
           renderOptions.removeChild(renderOptions.firstChild);
         }
       }
-      alert("Not quite my tempo... Hit OK to continue.");
       index++;
+      alert("Not quite my tempo... Hit OK to continue.");
       questionList();
       stopTimer();
       startTimer();
@@ -93,7 +92,8 @@ function questionList() {
     timer.style.display = "none";
     renderTime();
     renderQuestion.innerHTML = `Your chops score is ${finalScore}%!!`;
-    document.querySelector(".full-question").appendChild(document.querySelector(".scoreButton"));
+    submitScore();
+    submitScore.fired = true;
   }
 }
 
@@ -114,11 +114,82 @@ function optionsList() {
       if (e.target.innerHTML === answer) {
           finalScore += 10;
       }
-
+      alert = function() {};
       index++;
       questionList();
       stopTimer();
       startTimer();
     });
   }
+}
+
+let leaderboard = () => {
+    window.location.href = "/leaderboard.html";
+}
+
+init();
+
+function init() {
+
+    let storedScores = JSON.parse(localStorage.getItem("scores"));
+
+    if (storedScores !== null) {
+        scores = storedScores;
+    }
+
+    renderScores();
+}
+
+function renderScores() {
+    for (let i = 0; i < scores.length; i++) {
+        let score = scores[i];
+
+        let newRow = document.createElement("tr");
+        let newdata1 = document.createElement("td");
+        let newdata2 = document.createElement("td");
+
+        newRow.setAttribute("data-index", i);
+
+        newdata1.className = "bandname";
+        newdata2.className = "score";
+
+        newdata1.textContent = score.bandName;
+        newdata2.textContent = score.scoreNumber;
+        
+        document.querySelector("tbody").appendChild(newRow);
+        newRow.appendChild(newdata1);
+        newRow.appendChild(newdata2);
+    }
+}
+
+function storeScores() {
+    localStorage.setItem("scores", JSON.stringify(scores));
+}
+
+function submitScore() {
+    if (submitScore.fired) return;
+    let nameInput = questionStart.appendChild(document.createElement("input"));
+    nameInput.className = "nameInput";
+    nameInput.placeholder = "Band Name";
+    let submitBtn = document.createElement("button");
+    questionStart.append(submitBtn);
+    submitBtn.className = "scoreButton";
+    submitBtn.innerHTML = "Add Score";
+
+    submitBtn.addEventListener("click", function (event) {
+        event.preventDefault();
+    
+        localStorage.setItem("bandname", nameInput.value);
+        localStorage.setItem("score", finalScore);
+    
+        let score = {
+            bandName: nameInput.value.trim(),
+            scoreNumber: finalScore
+        }
+        alert("Score added!");
+        scores.push(score);
+        location.reload();
+        storeScores();
+        renderScores();
+    });
 }
